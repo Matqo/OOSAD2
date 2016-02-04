0d0f0348e13011b791fb59745e05a1d768cf23db
Matqo <martinfutas@gmail.com>
2016-02-04 18:14:57 +0000

    Commit 2

---
+++ Card.java
@@ -0,0 +1,21 @@
+public class Card {
+
+	private int rank;
+	private int suit;
+	private boolean faceUp;
+
+	/**
+	 * 
+	 * @param card
+	 */
+	public int compareTo(int card) {
+		// TODO - implement Card.compareTo
+		throw new UnsupportedOperationException();
+	}
+
+	public void flip() {
+		// TODO - implement Card.flip
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Hand.java
@@ -0,0 +1,15 @@
+public class Hand {
+
+	private card cards;
+	private int value;
+
+	/**
+	 * 
+	 * @param Hand
+	 */
+	public int compareTo(int Hand) {
+		// TODO - implement Hand.compareTo
+		throw new UnsupportedOperationException();
+	}
+
+}
