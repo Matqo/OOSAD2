e12dbc83f9176795c6fbcd4709c0b95110e187ae
Matqo <martinfutas@gmail.com>
2016-03-03 17:41:14 +0000

    Week 4

---
+++ Week4/CategoryA.java
@@ -0,0 +1,8 @@
+public class CategoryA implements SalaryCalculator {
+
+	public double getSalary() {
+		// TODO - implement CategoryA.getSalary
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Week4/CategoryB.java
@@ -0,0 +1,8 @@
+public class CategoryB implements SalaryCalculator {
+
+	public double getSalary() {
+		// TODO - implement CategoryB.getSalary
+		throw new UnsupportedOperationException();
+	}
+
+}
